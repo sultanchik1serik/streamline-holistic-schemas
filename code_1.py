6efa235dd2390fa8a29fe90ab3420e95830c5a33
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2024-05-01 15:13:45 -0400

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+        for (int i = 0; i < 10; i++) {
+
+        int[] data = new int[10];
+
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+}
+        }
+        }
+public class RandomDataGenerator {
+    }
